9cb41e14cc6f192e750057240759bef088b6e7e5
Tyler Boone <thboone@gmail.com>
2026-02-07 10:39:39 -0800

    add sample

---
+++ CodeLib/Samples/ClassSample.cs
@@ -0,0 +1,40 @@
+﻿// foo
+namespace CodeLib.Samples
+{
+    internal class ClassSample
+    {
+
+        public string Foo() => "Foo";
+
+        public string Bar() => Foo() + "Bar";
+
+        public bool Maybe { get; set; }
+        public string Baz() => Maybe ? Foo() : Bar();
+
+    }
+
+    namespace Foo
+    {
+        public class Mork
+        {
+            public int Foo() => 1 + 2;
+        }
+    }
+
+    namespace Bar.Baz
+    {
+        public enum Mork
+        {
+            Hello,
+            World
+        }
+    }
+
+    namespace Foo
+    {
+        public class Pickle
+        {
+            public int Bar() => 10 * 2;
+        }
+    }
+}
