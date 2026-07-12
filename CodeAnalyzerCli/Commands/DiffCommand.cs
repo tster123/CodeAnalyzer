5c2f2ff140a31571b29b371f8505066150c1065b
Tyler Boone <thboone@gmail.com>
2026-07-12 11:14:32 -0700

    moving to specter CLI

--- CodeAnalyzerCli/Commands/DiffCommand.cs
+++ CodeAnalyzerCli/Commands/DiffCommand.cs
@@ -1,16 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Text;
+﻿using System.ComponentModel;
 using Spectre.Console.Cli;
+
+namespace CodeAnalyzerCli.Commands;
 
 public class DiffCommand : Command<DiffOptions>
 {
+    protected override int Execute(CommandContext context, DiffOptions settings, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
 }
 
 public class DiffOptions : CommandSettings
 {
     [CommandArgument(0, "<filename>")] 
-    [Description("The name to greet")]
-    private string LeftFile;
+    [Description("Left (usually old) file")]
+    public required string LeftFile { get; set; }
+    
+    [CommandArgument(1, "<filename>")] 
+    [Description("Right (usually new) file")]
+    public required string RightFile { get; set; }
 }
---
+++ CodeAnalyzerCli/Commands/MetricsCommand.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+using CodeLib;
+using Spectre.Console.Cli;
+using Wrapped.System.IO;
+
+namespace CodeAnalyzerCli.Commands;
+
+public class MetricsCommand: Command<MetricsOptions>
+{
+    protected override int Execute(CommandContext context, MetricsOptions settings, CancellationToken cancellationToken)
+    {
+        CodeStreamer s = new(settings.PrintMetrics, settings.PrintAst);
+        s.ProcessFolder(new DirectoryInfoWrap(settings.Directory));
+        Console.WriteLine("Errors: " + s.Errors);
+        return 0;
+    }
+}
+
+public class MetricsOptions : CommandSettings
+{
+    [CommandArgument(0, "<directory>")] 
+    [Description("Code directory to run metrics on")]
+    public required string Directory { get; set; }
+    
+    [CommandOption("-m|--metrics")]
+    [Description("Generate metrics for the code in this folder")]
+    public bool PrintMetrics { get; init; }
+    
+    [CommandOption("-a|--ast")]
+    [Description("Print the abstract syntax tree (AST) of the code")]
+    public bool PrintAst { get; init; }
+}
